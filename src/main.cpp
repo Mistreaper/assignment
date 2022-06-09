6b08bf6dab9061db739eefef49e68a801ecf221b
Mistreaper <realmistreaper@gmail.com>
2022-06-09 09:43:09 +0700

    ms-vscode-cpptools

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include "collatzconjecture.h"
-#include <primenums.h>
+#include "primenums.h"
 
 int main() {
-    std::cout << "type value of counter: ";
-    int howMany;
-    std::cin >> howMany; 
-    unifiedMethod(howMany);
-    std::cout << "" << std::endl;
+    
     return 0;
+
 }
