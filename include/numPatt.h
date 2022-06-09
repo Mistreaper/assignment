9c9ac0706db51208b54e16bff740cf0e217976a8
Mistreaper <realmistreaper@gmail.com>
2022-06-09 14:53:21 +0700

    fibonacci

--- include/numPatt.h
+++ include/numPatt.h
@@ -2,20 +2,22 @@
 #define NUMPATT_H
 #include <iostream>
 
+// the next number is the sum of this number and the previous number.
 void fibonacci(int x) {
 
+    // this number
     int thn = 1;
+    // the previous number
     int pn = 0;
-    std::cout << pn << " ";
-    std::cout << thn << " "; 
+    // the sum of the two numbers, which is the next number 
     int nn;
     int counter = 1;
     while (counter <= x) {
+        std::cout << pn << " "; 
         nn = thn + pn;
         pn = thn;
         thn = nn;
         counter++;
-        std::cout << thn << " "; 
     }
 }
 
